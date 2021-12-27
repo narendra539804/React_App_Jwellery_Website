495c9fed420923d79c0203bc58d8b1fa6a5e7cc5
narendra539804 <64107227+narendra539804@users.noreply.github.com>
2021-12-27 22:39:54 +0530

    Update Add.js

--- src/components/Add.js
+++ src/components/Add.js
@@ -3,8 +3,26 @@ import React from "react";
 const Add = () => {
   return (
     <div>
-      <h1>Hi ra</h1>
-      <h2>Hi babe</h2>
+      <form>
+        <input type="file" placeholder="Upload Image" />
+        <br />
+        <input type="text" placeholder="Enter the Name" />
+        <br />
+        <input type="text" placeholder="Enter the code" />
+        <br />
+        <input type="text" placeholder="Enter the Weight" />
+        <br />
+        <input type="text" placeholder="Enter the Purity" />
+        <br />
+        <input type="text" placeholder="Enter the Making Charges" />
+        <br />
+        <input type="text" placeholder="Enter the Price" />
+        <br />
+        <button type="submit">Submit</button>
+        &nbsp;&nbsp;
+        <button type="submit"> Cancel</button>
+        <br />
+      </form>
     </div>
   );
 };
