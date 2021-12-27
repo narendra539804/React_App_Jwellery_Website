207e9462cfae85eb3410e50f9210b18e0050d462
narendra539804 <64107227+narendra539804@users.noreply.github.com>
2021-12-27 23:46:12 +0530

    Update Add.js

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,24 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Add = () => {
+  const [data, setData] = useState({
+    filename: "",
+    name: "",
+    code: "",
+    weight: "",
+    purity: "",
+    charges: "",
+    price: "",
+  });
+  const { filename, name, code, weight, purity, charges, price } = data;
+  const changeHandler = (e) => {
+    setData({ ...data, [e.target.name]: [e.target.value] });
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
   return (
     <div>
-      <form>
-        <input type="file" placeholder="Upload Image" />
+      <form onSubmit={submitHandler}>
+        <input type="file" />
         <br />
-        <input type="text" placeholder="Enter the Name" />
+        <input
+          type="text"
+          placeholder="Enter the Name"
+          name="name"
+          value={name}
+          onChange={changeHandler}
+        />
         <br />
-        <input type="text" placeholder="Enter the code" />
+        <input
+          type="text"
+          placeholder="Enter the code"
+          name="code"
+          value={code}
+          onChange={changeHandler}
+        />
         <br />
-        <input type="text" placeholder="Enter the Weight" />
+        <input
+          type="text"
+          placeholder="Enter the Weight"
+          name="weight"
+          value={weight}
+          onChange={changeHandler}
+        />
         <br />
-        <input type="text" placeholder="Enter the Purity" />
+        <input
+          type="text"
+          placeholder="Enter the Purity"
+          name="purity"
+          value={purity}
+          onChange={changeHandler}
+        />
         <br />
-        <input type="text" placeholder="Enter the Making Charges" />
+        <input
+          type="text"
+          placeholder="Enter the Making Charges"
+          value={charges}
+          onChange={changeHandler}
+          name="charges"
+        />
         <br />
-        <input type="text" placeholder="Enter the Price" />
+        <input
+          type="text"
+          placeholder="Enter the Price"
+          name="price"
+          value={price}
+          onChange={changeHandler}
+        />
         <br />
-        <button type="submit">Submit</button>
+        <button type="submit" name="submit">
+          Submit
+        </button>
         &nbsp;&nbsp;
         <button type="submit"> Cancel</button>
         <br />
